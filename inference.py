c4126616f8fcf05984b2921476af6386ec3b6ae5
Rahul Dey <rahuldey91@gmail.com>
2024-11-20 21:29:20 -0800

    added optical flow based frame selection for interpolation

--- inference.py
+++ inference.py
@@ -7,6 +7,7 @@ from omegaconf import OmegaConf
 from main import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from torchvision.io import read_video, write_video
+from RAFT.raft_bi import RAFT_bi
 
 
 parser = argparse.ArgumentParser(description='Frame Interpolation Evaluation')
@@ -26,7 +27,9 @@ parser.add_argument('--ddim_steps', type=int, default=200)
 def main():
     args = parser.parse_args()
 
-    # initialise model
+    '''
+    Initialize model
+    '''
     config = OmegaConf.load(args.config)
     model = instantiate_from_config(config.model)
     model.load_state_dict(torch.load(args.ckpt)['state_dict'])
@@ -35,6 +38,12 @@ def main():
     model = model.eval()
     print('Model loaded successfully')
 
+    # initialize optical flow model
+    raft = RAFT_bi("pretrained/raft-things.pth")
+
+    '''
+    Function to generate a single interpolated frame between any two given frames
+    '''
     def interpolate(frame0, frame1):
         if len(frame0.shape) < 4:
             frame0 = frame0[None]
@@ -56,6 +65,9 @@ def main():
                 out =  torch.clamp(out, min=-1., max=1.) # interpolated frame in [-1,1]
         return out
 
+    '''
+    Function to perform recursive binary-search based interpolation between any two given frames
+    '''
     def binary_fill(frames, frame0_idx, frame1_idx):
         if frame0_idx >= frame1_idx-1:
             return
@@ -67,7 +79,9 @@ def main():
         binary_fill(frames, frame0_idx, new_idx)
         binary_fill(frames, new_idx, frame1_idx)
 
-    # set up sampler
+    '''
+    Initialize sampler
+    '''
     if args.use_ddim:
         ddim = DDIMSampler(model)
         sample_func = partial(ddim.sample, S=args.ddim_steps, eta=args.ddim_eta, verbose=False)
@@ -90,6 +104,9 @@ def main():
     # Start interpolation
     print('Using model {} to upsample file {}'.format(args.net, fname))
 
+    '''
+    Given constraints: initial and final timestamps of frameA and frameB
+    '''
     # Original and new timestamps of frames to be rearranged
     frameA_orig_t = 16
     frameA_new_t = 5
@@ -108,23 +125,80 @@ def main():
     # frame A inserted to new position
     out_frames[frameA_new_idx] = stream[frameA_orig_idx]
 
-    # interpolate between first segment end and frameA
-    binary_fill(out_frames, frameA_new_idx-interp_gap, frameA_new_idx)
-    # interpolate between frameA and second segment start
-    binary_fill(out_frames, frameA_new_idx, frameA_new_idx+interp_gap)
-
     # frame B inserted to new position
     out_frames[frameB_new_idx] = stream[frameB_orig_idx]
 
-    # interpolate between second segment end and frame B
-    binary_fill(out_frames, frameB_new_idx-interp_gap, frameB_new_idx)
-    #interpolate between frameB and last segment start
-    binary_fill(out_frames, frameB_new_idx, frameB_new_idx+interp_gap)
+    # downsample the edited video to compute optical flows between inserted frames and other frames
+    stream_down = torch.nn.functional.interpolate(out_frames, scale_factor=(0.5, 0.5))
+    stream_down = stream_down[None].permute(0, 2, 1, 3, 4)
+
+    '''
+    In order to find the closest frame before frameA using optical flow, we compute the optical flow 
+    between frameA and every other frame before frameA's news position until 30 frames. The frame with the least
+    average optical flow is selected as the starting frame for interpolation until frameA
+    ''' 
+    flows = torch.zeros([28]).cuda()
+    for i in range(2, 30):
+        with torch.no_grad():
+            # compute forward and backward optical flow between frameA and frame[i]
+            flow_fwd, flow_back = raft.forward_slicing(stream_down[:,:,[frameA_new_idx-i, frameA_new_idx]])
+        # compute the mean optical flow amplitude
+        flows[i-2] = flow_fwd.norm(dim=1).mean() + flow_back.norm(dim=1).mean()
+    # select the frame with the least mean optical flow amplitude as the starting frame for interpolation
+    closest_before_frameA = flows.argmin().item()+2
+    # interpolate between closest frame and frameA
+    print(f"Interpolating between frame {frameA_new_idx-closest_before_frameA} and frame {frameA_new_idx}")
+    binary_fill(out_frames, frameA_new_idx-closest_before_frameA, frameA_new_idx)
+
+    # find the closest frame after frameA using optical flow
+    flows = torch.zeros([28]).cuda()
+    for i in range(2, 30):
+        with torch.no_grad():
+            # compute forward and backward optical flow between frameA and frame[i]
+            flow_fwd, flow_back = raft.forward_slicing(stream_down[:,:,[frameA_new_idx, frameA_new_idx+i]])
+        # compute the mean optical flow amplitude
+        flows[i-2] = flow_fwd.norm(dim=1).mean() + flow_back.norm(dim=1).mean()
+    # select the frame with the least mean optical flow amplitude as the ending frame for interpolation
+    closest_after_frameA = flows.argmin() + 2
+    # interpolate between frameA and closest frame
+    print(f"Interpolating between frame {frameA_new_idx} and frame {frameA_new_idx+closest_after_frameA}")
+    binary_fill(out_frames, frameA_new_idx, frameA_new_idx+closest_after_frameA)
+
+    '''
+    Frame B inserted and interpolated in the same way as frame A
+    '''
+    # find the closest frame before frameB using optical flow
+    flows = torch.zeros([28]).cuda()
+    for i in range(2, 30):
+        with torch.no_grad():
+            # compute forward and backward optical flow between frameB and frame[i]
+            flow_fwd, flow_back = raft.forward_slicing(stream_down[:,:,[frameB_new_idx-i, frameB_new_idx]])
+        # compute the mean optical flow amplitude
+        flows[i-2] = flow_fwd.norm(dim=1).mean() + flow_back.norm(dim=1).mean()
+    # select the frame with the least mean optical flow amplitude as the starting frame for interpolation
+    closest_before_frameB = flows.argmin().item()+2
+    # interpolate between closest frame and frame B
+    print(f"Interpolating between frame {frameB_new_idx-closest_before_frameB} and frame {frameB_new_idx}")
+    binary_fill(out_frames, frameB_new_idx-closest_before_frameB, frameB_new_idx)
+
+    # find the closest frame after frameB using optical flow
+    flows = torch.zeros([28]).cuda()
+    for i in range(2, 30):
+        with torch.no_grad():
+            # compute forward and backward optical flow between frameB and frame[i]
+            flow_fwd, flow_back = raft.forward_slicing(stream_down[:,:,[frameB_new_idx, frameB_new_idx+i]])
+        # compute the mean optical flow amplitude
+        flows[i-2] = flow_fwd.norm(dim=1).mean() + flow_back.norm(dim=1).mean()
+    # select the frame with the least mean optical flow amplitude as the ending frame for interpolation
+    closest_after_frameB = flows.argmin() + 2
+    #interpolate between frameB and the closest frame
+    print(f"Interpolating between frame {frameB_new_idx} and frame {frameB_new_idx+closest_after_frameB}")
+    binary_fill(out_frames, frameB_new_idx, frameB_new_idx+closest_after_frameB)
 
     # write output file
     out_frames = (out_frames * 0.5 + 0.5) * 255
     out_frames = out_frames.to(torch.uint8).permute(0,2,3,1).cpu()
-    outname = '{}_interp_{}.mp4'.format(fname, interp_gap)
+    outname = '{}_interp_optical.mp4'.format(fname, interp_gap)
     write_video(outname, out_frames, fps=fps)
     print('Output written to {}'.format(outname))
 
